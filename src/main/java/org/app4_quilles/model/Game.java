9ead086c37c747bbfde4eeff9327762f4871a1af
Olivier Proquez <olivier.proquez@u-psud.fr>
2023-12-21 17:15:56 +0100

    firsts tests for Game class and basic architecture of Game

--- src/main/java/org/app4_quilles/model/Game.java
+++ src/main/java/org/app4_quilles/model/Game.java
@@ -1,4 +1,48 @@
 package org.app4_quilles.model;
 
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.HashMap;
+
 public class Game {
+    //Attributes
+    private boolean started;
+    private int amountOfPlayers;
+    private ArrayList listNames;
+    private HashMap<String, Integer> scores;
+
+    //Constructors
+    public Game(){
+        this.started = false;
+        this.amountOfPlayers = 0;
+        this.listNames = new ArrayList<>();
+        this.scores = new HashMap<String, Integer>();
+    }
+
+    //Getters
+    public boolean getStarted(){
+        return this.started;
+    }
+    public int getAmountOfPlayers(){
+        return this.amountOfPlayers;
+    }
+    public ArrayList getListNames(){
+        return this.listNames;
+    }
+    public HashMap<String, Integer> getPlayersScores(){
+        return new HashMap<String, Integer>();
+    }
+    public int getPlayerScore(String name){
+        return 0;
+    }
+
+    //Setters
+    public void setStarted(boolean bool_start){
+         this.started = bool_start;
+    }
+    //Methods
+    public void addPlayer(String name){}
+    public void addScore(String name, Integer score){}
+
+    public void startGame(){}
 }
---
+++ src/test/java/org/app4_quilles/GameTest.java
@@ -0,0 +1,50 @@
+package org.app4_quilles;
+
+import java.util.ArrayList;
+
+import org.app4_quilles.model.Game;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameTest {
+
+    @Test
+    public void testStarted() {
+        Game gameTest = new Game();
+        assertEquals("Game.started should be false", gameTest.getStarted(), false);
+        gameTest.setStarted(true);
+        assertEquals("Game.started should be true", gameTest.getStarted(), true);
+    }
+
+    @Test
+    public void testNoPlayersAtStartOfGame() {
+        Game gameTest = new Game();
+        assertEquals(gameTest.getAmountOfPlayers(), 0);
+        assertEquals(gameTest.getListNames().size(), 0);
+        assertEquals(gameTest.getPlayersScores(), 0);
+    }
+
+    @Test
+    public void testAddPlayer() {
+        Game gameTest = new Game();
+        gameTest.setStarted(true);
+        gameTest.addPlayer("Olivier");
+        ArrayList<String> emptyList = new ArrayList<>();
+        emptyList.add("Olivier");
+
+        assertEquals("Only Olivier in listNames", emptyList, gameTest.getListNames());
+        assertEquals("Only Olivier in listNames", gameTest.getListNames().size(), 1);
+        assertEquals("initial Olivier score should be 0", gameTest.getPlayerScore("Olivier"), 0);
+        gameTest.addScore("Olivier", 10);
+        assertEquals("Olivier's score should be 10", gameTest.getPlayerScore("Olivier"), 10);
+        System.out.println(gameTest.getPlayerScore("Olivier"));
+    }
+
+    @Test
+    public void testAddPlayer() {
+        Game gameTest = new Game();
+        assertEquals();
+    }
+
+}
