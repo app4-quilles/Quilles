5c7a3a46cd01fdaa8b8fea74bf8c53e41239248d
Maxime <maxime.le-bot@universite-paris-saclay.fr>
2024-01-16 08:56:55 +0100

    Update Score.java
    Added the possibility to have a number of rows different from 10, and removed the tests to move them to MainTest

--- src/main/java/org/app4_quilles/model/Score.java
+++ src/main/java/org/app4_quilles/model/Score.java
@@ -2,9 +2,11 @@ package org.app4_quilles.model;
 
 public class Score {
 	private int numberOfPins;
-	private int[][] scoreTab = new int[10][2];
+	private int numberOfRounds;
+	private int[][] scoreTab;
 	
 	public Score() {
+		scoreTab = new int[10][2];
 		for (int i = 0; i < scoreTab.length; i++) {
 	        for (int j = 0; j < scoreTab[i].length; j++) {
 	        	scoreTab[i][j] = 0;
@@ -13,8 +15,9 @@ public class Score {
 		numberOfPins = 10;
 	}
 	public Score(int[][] pins, int lastShot) {
-		for (int i = 0; i < scoreTab.length; i++) {
-	        for (int j = 0; j < scoreTab[i].length; j++) {
+		scoreTab = new int[pins.length][2];
+		for (int i = 0; i < pins.length; i++) {
+	        for (int j = 0; j < 2; j++) {
 	        	scoreTab[i][j] = 0;
 	        }
 	    }
@@ -22,6 +25,7 @@ public class Score {
 		calculScore(pins, lastShot);
 	}
 	public Score(int nb, int[][] pins, int lastShot) {
+		scoreTab = new int[pins.length][2];
 		for (int i = 0; i < scoreTab.length; i++) {
 	        for (int j = 0; j < scoreTab[i].length; j++) {
 	        	scoreTab[i][j] = 0;
@@ -60,89 +64,5 @@ public class Score {
 	        total += res;
 	        scoreTab[i][1] = total;
 	    }
-	}
-	
-	public boolean testing() {
-		boolean res = true;
-		int[][] playerPins = new int[10][2];
-		//every shot is a strike
-		for (int i = 0; i < playerPins.length; i++) {
-			playerPins[i][0] = numberOfPins;
-		}
-		playerPins[9][1] = numberOfPins;
-		calculScore(playerPins, numberOfPins);
-		for (int i = 0; i < playerPins.length; i++) {
-			res = res && scoreTab[i][0] == numberOfPins * 3;
-			res = res && scoreTab[i][1] == numberOfPins * 3 * (i + 1);
-		}
-		
-		//every shot is a strike except for last shot, which is 0
-		calculScore(playerPins, 0);
-		for (int i = 0; i < playerPins.length - 1; i++) {
-			res = res && scoreTab[i][0] == numberOfPins * 3;
-			res = res && scoreTab[i][1] == numberOfPins * 3 * (i + 1);
-		}
-		res = res && scoreTab[9][0] == numberOfPins * 2;
-		res = res && scoreTab[9][1] == numberOfPins * 3 * (9 + 1) - numberOfPins;
-		
-		//every shot is a strike, except for before last shot, which is 0.
-		playerPins[9][1] = 0;
-		calculScore(playerPins, numberOfPins);
-		for (int i = 0; i < playerPins.length - 2; i++) {
-			res = res && scoreTab[i][0] == numberOfPins * 3;
-			res = res && scoreTab[i][1] == numberOfPins * 3 * (i + 1);
-		}
-		res = res && scoreTab[9][0] == numberOfPins * 2;
-		res = res && scoreTab[9][1] == numberOfPins * 3 * (9) - numberOfPins;
-		res = res && scoreTab[9][0] == numberOfPins * 2;
-		res = res && scoreTab[9][1] == numberOfPins * 3 * (10) - numberOfPins * 2;
-
-		//every shot is a strike, except for before last shot, which is 0.
-		playerPins[9][1] = 0;
-		calculScore(playerPins, numberOfPins);
-		for (int i = 0; i < playerPins.length - 2; i++) {
-			res = res && scoreTab[i][0] == numberOfPins * 3;
-			res = res && scoreTab[i][1] == numberOfPins * 3 * (i + 1);
-		}
-		res = res && scoreTab[9][0] == numberOfPins * 2;
-		res = res && scoreTab[9][1] == numberOfPins * 3 * (9) - numberOfPins;
-		res = res && scoreTab[9][0] == numberOfPins * 2;
-		res = res && scoreTab[9][1] == numberOfPins * 3 * (10) - numberOfPins * 2;
-
-		//every shot is 0.
-		for (int i = 0; i < playerPins.length; i++) {
-			playerPins[i][0] = 0;
-			playerPins[i][1] = 0;
-		}
-		calculScore(playerPins, 0);
-		for (int i = 0; i < playerPins.length; i++) {
-			res = res && scoreTab[i][0] == 0;
-			res = res && scoreTab[i][1] == 0;
-		}
-		
-		//every shot is 0, exept for the first round which is a spare.
-		playerPins[0][0] = 6;
-		playerPins[0][1] = 4;
-		calculScore(playerPins, 0);
-		res = res && scoreTab[0][0] == 10;
-		res = res && scoreTab[0][1] == 10;
-		for (int i = 1; i < playerPins.length; i++) {
-			res = res && scoreTab[i][0] == 0;
-			res = res && scoreTab[i][1] == 10;
-		}
-		
-		//every shot is 0, exept for the first 2 rounds which are spares.
-		playerPins[1][0] = 5;
-		playerPins[1][1] = 5;
-		calculScore(playerPins, 0);
-		res = res && scoreTab[0][0] == 15;
-		res = res && scoreTab[0][1] == 15;
-		res = res && scoreTab[1][0] == 10;
-		res = res && scoreTab[1][1] == 25;
-		for (int i = 2; i < playerPins.length; i++) {
-			res = res && scoreTab[i][0] == 0;
-			res = res && scoreTab[i][1] == 25;
-		}
-		return res;
 	}
 }
