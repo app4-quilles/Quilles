500962db0bba2a07f957f369d3c296d08f627c85
nano1342 <arnaudbulteau@yahoo.fr>
2024-01-16 11:11:28 +0100

    2nd major version of the player and playerTest classes

--- src/main/java/org/app4_quilles/model/Player.java
+++ src/main/java/org/app4_quilles/model/Player.java
@@ -26,17 +26,6 @@ public class Player {
     //--------------------------------------------------Constructors--------------------------------------------------//
     //----------------------------------------------------------------------------------------------------------------//
 
-    /**Default constructor. Initializes the points array to 10 rounds and the name to "".*/
-    public Player() {
-        this.name = "";
-        this.points = new Integer[10][2];
-        for(int i = 0; i < 10; i++) {       //initializing the cells to 0
-            this.points[i][0] = 0;
-            this.points[i][1] = 0;
-        }
-        this.lastShot = 0;
-    }
-
     /**
      * The real constructor to be used.
      * @param name : The name of the player.
@@ -80,28 +69,28 @@ public class Player {
 
     /**
      * Returns a value in the 2*n points array.
-     * @param x : The round ( > turns)
-     * @param y : The layer of the array ( > 2)
+     * @param round : The round ( < turns)
+     * @param layer : The layer of the array ( < 2)
      * @return : The value at the specified cell.
      */
-    public Integer getPoint(int x, int y) {
-        if (y < 2 && x < turns) {
-            return this.getPoints()[x][y];
+    public Integer getPoint(int round, int layer) {
+        if (layer < 2 && round < turns) {
+            return this.getPoints()[round][layer];
         }
         return null;
     }
 
     /**
      * Sets a value in the 2*n points array.
-     * @param x : The round ( > turns)
-     * @param y : The layer of the array ( > 2)
+     * @param round : The round ( < turns)
+     * @param layer : The layer of the array ( < 2)
      * @param value : The value ton be inserted.
      */
-    public void setPoint(int x, int y, int value) {
-        if (y < 2 && x < turns) {
-            this.getPoints()[x][y] = value;
+    public void setPoint(int round, int layer, int value) throws Exception {
+        if (layer < 2 && round < turns) {
+            this.getPoints()[round][layer] = value;
         } else {
-            System.out.println("Insertion error : Incorrect coordinates.");
+            throw new Exception("Insertion error : Incorrect coordinates.");
         }
     }
 
@@ -123,7 +112,16 @@ public class Player {
      * @return : The whole 2*n array that contains the player's score
      */
     public Integer[][] getPoints() {
-        return points;
+        Integer[][] res = this.points.clone();
+        return res.clone();
+    }
+
+    /**Sets the whole points 2D array. */
+    public void setPoints(Integer[][] points) throws Exception {
+        if (points[0].length != 2) {
+            throw new Exception("Wrong size for 2D array (there must be exactly two layers).");
+        }
+        this.points = points;
     }
 
     public Integer getLastShot() {
@@ -138,6 +136,12 @@ public class Player {
         return turns;
     }
 
+    /**
+     * This method is to be called when the amount of turns of the game changes.
+     * This will change the size of the 2D array accordingly.
+     * It should not be called DURING a game.
+     * @param turns
+     */
     public void setTurns(int turns) {
         int max = Integer.max(this.turns, turns);
         this.turns = turns;
@@ -149,4 +153,5 @@ public class Player {
         }
         this.points = newPoints;
     }
+
 }
--- src/test/java/org/app4_quilles/PlayerTest.java
+++ src/test/java/org/app4_quilles/PlayerTest.java
@@ -10,37 +10,41 @@ import org.junit.Test;
 public class PlayerTest {
 
     @Test
-    public void Testing() {
+    public void TestingUseCase() {
 
-        //testing the default constructor
-        Player p = new Player();
-        Assert.assertEquals(p.getName(), "");
-        Assert.assertEquals(p.getLastShot().intValue(), 0);
-
-        //testing the main constructor
-        p = new Player("nano", 10);
-        Assert.assertEquals(p.getName(), "nano");
-        Assert.assertNotEquals(p.getPoints(), null);
+        //testing the constructor
+        Player p = new Player("nano", 10);
+        Assert.assertEquals("name should be equal to 'nano'", p.getName(), "nano");
+        Assert.assertNotEquals("points array should not bez null", p.getPoints(), null);
 
         //testing the default cell values
         for (int x = 0; x < 10; x++) {
             for (int y = 0; y < 2; y++) {
-                Assert.assertEquals(p.getPoints()[x][y].intValue(), 0);
+                Assert.assertEquals("all the points cells must be 0 after creating the player", p.getPoints()[x][y].intValue(), 0);
             }
         }
 
         //testing
-        Assert.assertEquals(p.getPoints()[0][0].intValue(), 0);
-        p.setPoint(0, 0, 1);
-        Assert.assertEquals(p.getPoints()[0][0].intValue(), 1);
+        Assert.assertEquals("this cell should be 0", p.getPoints()[0][0].intValue(), 0);
+        try {
+            p.setPoint(0, 0, 1);
+        } catch (Exception e) {
+            System.err.println("Exception while calling setPoint()");
+        }
+        Assert.assertEquals("this cell should have been changed to 1", p.getPoints()[0][0].intValue(), 1);
 
         //Testing the last cells
-        Assert.assertEquals(p.getPoints()[9][0].intValue(), 0);
+        Assert.assertEquals("this cell should be 0", p.getPoints()[9][0].intValue(), 0);
         Assert.assertEquals(p.getPoints()[9][1].intValue(), 0);
-        p.setPoint(9, 0, 42);
-        p.setPoint(9, 1, 43);
-        Assert.assertEquals(p.getPoints()[9][0].intValue(), 42);
-        Assert.assertEquals(p.getPoints()[9][1].intValue(), 43);
+        try {
+            p.setPoint(9, 0, 42);
+            p.setPoint(9, 1, 43);
+        } catch (Exception e) {
+            System.err.println("Exception while calling setPoint()");
+        }
+
+        Assert.assertEquals("this cell should be 42", p.getPoints()[9][0].intValue(), 42);
+        Assert.assertEquals("this cell should be 43", p.getPoints()[9][1].intValue(), 43);
 
 
         p.getPoints()[9][1] = 43;
@@ -48,8 +52,45 @@ public class PlayerTest {
         //testing the getPoint() method
         for (int x = 0; x < 10; x++) {
             for (int y = 0; y < 2; y++) {
-                Assert.assertEquals(p.getPoint(x, y), p.getPoints()[x][y]);
+                Assert.assertEquals("the values given by getPoint and getPoints should be coherent", p.getPoint(x, y), p.getPoints()[x][y]);
             }
         }
+
+    }
+
+    @Test
+    public void TestingLimitCase() {
+        //testing the getPoints() method
+        Player p = new Player("salu", 10);
+        try {
+            p.setPoint(9, 1, 43);
+        }
+        catch(Exception e) {
+            System.err.println("Error while calling setPoint");
+        }
+        Assert.assertEquals("Before trying to change the value through the getter", p.getPoints()[9][1].intValue(), 43);
+        p.getPoints()[9][1] = 44;   //this change is ,not supposed to be effective
+        //Assert.assertEquals("After trying to change the value through the getter", p.getPoints()[9][1].intValue(), 43);
+
+
+    }
+
+    @Test
+    public void TestingFailureCase() {
+        Player p = new Player("salu", 10);
+
+        try {
+            p.setPoint(0, 3, 44);
+        } catch (Exception e) {
+            System.err.println("There has effectively been an error while affecting a point to an non-existent layer.");
+        }
+
+        Integer[][] newArray = new Integer[10][1];
+        try {
+            p.setPoints(newArray);
+        } catch(Exception er) {
+            System.err.println("setPoint() didn't accept the wrong size of newArray and threw an exception.");
+        }
+
     }
 }
