16049bc00047b80fdae16b50e0cfced4a8b6e909
Olivier Proquez <olivier.proquez@gmail.com>
2024-01-17 22:09:20 +0100

    completed all tests and game classes

--- src/main/java/org/app4_quilles/model/Game.java
+++ src/main/java/org/app4_quilles/model/Game.java
@@ -1,45 +1,80 @@
 package org.app4_quilles.model;
 
 import java.util.ArrayList;
-import java.util.Dictionary;
 import java.util.HashMap;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Game {
     //Attributes
+    private int amountOfPins;
+    private int amountOfTurns;
     private boolean started;
     private int amountOfPlayers;
-    private ArrayList listNames;
-    private HashMap<String, Integer> scores;
+    private ArrayList<Player> listPlayers;
+    private HashMap<Player, Score> scores;
+    private HashMap<Player, int[][]> pinsMap; //contains the history of touched pins
 
     //Constructors
     public Game(){
         this.started = false;
+        this.amountOfTurns = 10;
+        this.amountOfPins = 10;
         this.amountOfPlayers = 0;
-        this.listNames = new ArrayList<>();
-        this.scores = new HashMap<String, Integer>();
+        this.listPlayers = new ArrayList<>();
+        this.scores = new HashMap<Player, Score>();
+        this.pinsMap = new HashMap<Player, int[][]>();
     }
 
     //Getters
-    public boolean getStarted(){
-        return this.started;
-    }
-    public int getAmountOfPlayers(){
-        return this.amountOfPlayers;
-    }
-    public ArrayList getListNames(){
-        return this.listNames;
-    }
-    public HashMap<String, Integer> getPlayersScores(){
-        return new HashMap<String, Integer>();
-    }
-    public int getPlayerScore(String name){
-        return 0;
+    public int getAmountOfPins() {
+        return amountOfPins;
     }
 
+    public int getAmountOfTurns() {
+        return amountOfTurns;
+    }
+
+    public boolean getStarted() {
+        return started;
+    }
+
+    public int getAmountOfPlayers() {
+        return amountOfPlayers;
+    }
+
+    public ArrayList<Player> getListPlayers() {
+        return listPlayers;
+    }
+
+    public HashMap<Player, Score> getScores() {
+        return scores;
+    }
+
+    public HashMap<Player, int[][]> getPinsMap() {
+        return pinsMap;
+    }
+
+    //Setters
+    public void setAmountOfPins(int amountOfPins) {
+        this.amountOfPins = amountOfPins;
+    }
+
+    public void setAmountOfTurns(int amountOfTurns) {
+        this.amountOfTurns = amountOfTurns;
+    }
+
+    public void setStarted(boolean bool_start) {
+        this.started = bool_start;
+    }
+
+    public void setAmountOfPlayers(int amountOfPlayers) {
+        this.amountOfPlayers = amountOfPlayers;
+    }
+
+
+
     //Scanner for standard input file (a temp method that will be replaced by Charly's one
-    public String getInputString(String promptMsg) {
+    public static String getInputString(String promptMsg) { //TODO test by sending text through System.in (ask Charly Schmidt at next standup)
         String response = null;
         Scanner scanner = new Scanner(System.in);
         System.out.print(promptMsg + ": ");
@@ -49,18 +84,15 @@ public class Game {
         return response;
     }
 
-    //Setters
-    public void setStarted(boolean bool_start){
-         this.started = bool_start;
-    }
+
 
     //Methods
     public void addPlayer(String name){
-        
+        Player newPlayer = new Player(name, this.amountOfTurns);
+        this.listPlayers.add(newPlayer);
     }
-    public void addScore(String name, Integer score){}
 
-    public boolean isInt(String s){
+    public static boolean isInt(String s){
         {
             try
             { int i = Integer.parseInt(s); return true; }
@@ -69,21 +101,157 @@ public class Game {
             { return false; }
         }
     }
-    public void play(){
-        System.out.println("Game started.");
+
+    public static int[][] getNewArray(int turns){
+        int[][] res = new int[turns][2];
+        for(int x = 0; x < turns; x++){
+            for(int y = 0; y < 2; y++) {
+                res[x][y] = 0;
+            }
+        }
+        return res;
+    }
+
+    public static int retrieveInt(String s){
         String amount = "";
         while(! isInt(amount)){
-            System.out.println("Enter amount of players (must be a number)");
-            amount = this.getInputString("");
+            System.out.print(s);
+            amount = getInputString("");
         }
-        this.amountOfPlayers = Integer.valueOf(amount);
+        return Integer.valueOf(amount);
+    }
+
+    //Useful for debugging  -> no tests needed as it is never used in final build
+    public int[][] integerArrayToIntArray(Integer[][] input){
+        int[][] res = new int[input.length][input[0].length];
+        for(int x = 0; x < input.length; x++){
+            for(int y = 0; y < input[0].length; y++) {
+                res[x][y] = input[x][y];
+            }
+        }
+        return res;
+    }
+
+    public static Integer[][] intArrayToIntegerArray(int[][] input){
+        Integer[][] res = new Integer[input.length][input[0].length];
+        for(int x = 0; x < input.length; x++){
+            for(int y = 0; y < input[0].length; y++) {
+                res[x][y] = input[x][y];
+            }
+        }
+        return res;
+    }
+
+    //Useful for debugging
+    public void stringIntArray(int[][] input){
+        for(int x = 0; x < input.length; x++){
+            for(int y = 0; y < input[0].length; y++) {
+                System.err.print(input[x][y]);
+            }
+            System.err.println("");
+        }
+    }
+
+    //Useful for debugging
+    public void stringIntegerArray(Integer[][] input){
+        for(int x = 0; x < input.length; x++){
+            for(int y = 0; y < input[0].length; y++) {
+                System.err.print(input[x][y]);
+            }
+            System.err.println("");
+        }
+    }
+
+    public void play(){
+        //Game setup
+        System.out.println("Game setup.");
+
+        this.amountOfTurns = retrieveInt("Enter amount of turns (must be a number)");
+        System.out.println("Amount of turns : "+this.amountOfTurns);
+
+        this.amountOfPins = retrieveInt("Enter amount of pins (must be a number)");
+        System.out.println("Amount of pins : "+this.amountOfPins);
+
+        this.amountOfPlayers = retrieveInt("Enter amount of players (must be a number)");
         System.out.println("Amount of players : "+this.amountOfPlayers);
+
+        //Asking user to enter players names. Those are turned into Player instances and stored in this.listPlayers
         for (int i = 0; i <  this.amountOfPlayers; i++) {
             String newPlayerName = this.getInputString("Enter new player ");
-            System.out.println(newPlayerName);
             this.addPlayer(newPlayerName);
         }
 
+
+        //Initializing score calculator for each player
+        for (Player p : this.listPlayers) {
+            Score sc = new Score(this.amountOfPins, getNewArray(this.amountOfTurns), 0);
+            this.scores.put(p, sc);
+
+            this.pinsMap.put(p, getNewArray(this.amountOfTurns));
+        }
+
+        //Game rounds
+        System.out.println("Game started");
+        int firstPinsTouched = 0;
+        int secondPinsTouched = 0;
+        for (int turn = 0; turn <  this.amountOfTurns; turn++) {
+            for (Player p : this.listPlayers) {
+                System.out.println("score beginning turn " + turn+1 + " : " + p.toString());
+                firstPinsTouched = 0;
+                secondPinsTouched = 0;
+                Integer[][] currentPlayerPoints = p.getPoints();
+                Score playerScore = this.scores.get(p);
+
+
+                firstPinsTouched = retrieveInt("First shot of " + p.getName() + " : enter pins touched (must be a number)");
+                this.pinsMap.get(p)[turn][0] = firstPinsTouched;
+                playerScore.calculScore(this.pinsMap.get(p), 0);
+                //stringIntArray(playerScore.getScoreTab());//debug
+                currentPlayerPoints = intArrayToIntegerArray(playerScore.getScoreTab());
+                //stringIntegerArray(currentPlayerPoints);
+                try{
+                    p.setPoints(currentPlayerPoints);
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+
+
+
+                //if there is a strike on the 1st shot there is no 2nd shot
+                if (firstPinsTouched == this.amountOfPins){
+                    this.pinsMap.get(p)[turn][1] = 0;
+                    System.out.println("STRIKE !!!!!!!!!!!!!!!!!!!!!!!!!!");
+                    System.out.println("score end turn " + turn+1 + " : " + p.toString());
+                    continue;
+                }
+                secondPinsTouched = retrieveInt("Second shot of " + p.getName() + " : enter pins touched (must be a number)");
+                this.pinsMap.get(p)[turn][1] = secondPinsTouched;
+                playerScore.calculScore(this.pinsMap.get(p), 0);
+                currentPlayerPoints = intArrayToIntegerArray(playerScore.getScoreTab());
+                try{
+                    p.setPoints(currentPlayerPoints);
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+                System.out.println("score end turn " + turn+1 + " : " + p.toString());
+            }
+        }
+
+        Player winner = this.listPlayers.get(0);
+        for(Player p : this.listPlayers){
+            if(p.getPoint(this.amountOfTurns - 1, 1)>winner.getPoint(this.amountOfTurns - 1, 1))
+            { //TODO amountofturns : remove - 1 with Maxim's update of scoring system
+                winner = p;
+            }
+        }
+        System.out.println("The winner is " + winner.getName() + "with " + winner.getPoint(this.amountOfTurns - 1, 1) + " points.");
     }
 
+
+    public static void main(String[] args) {
+        Game gameTest1 = new Game();
+        gameTest1.play();
+    }
 }
--- src/test/java/org/app4_quilles/GameTest.java
+++ src/test/java/org/app4_quilles/GameTest.java
@@ -1,52 +1,90 @@
 package org.app4_quilles;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.app4_quilles.model.Game;
+import org.app4_quilles.model.Player;
+import org.app4_quilles.model.Score;
+import org.junit.Assert;
 import org.junit.Test;
 
+import static org.app4_quilles.model.Game.*;
 import static org.junit.Assert.*;
 
 public class GameTest {
 
     @Test
-    public void testStarted() {
+    public void testConstructor() {
         Game gameTest = new Game();
-        String rep = gameTest.getInputString("test");
-        System.err.println(rep);
-        assertEquals("Game.started should be false", gameTest.getStarted(), false);
-        gameTest.setStarted(true);
-        assertEquals("Game.started should be true", gameTest.getStarted(), true);
+        assertFalse("started boolean should be false", gameTest.getStarted());
+        Assert.assertEquals("amount of turns should be 10", 10, gameTest.getAmountOfTurns());
+        Assert.assertEquals("amount of pins should be 10", 10, gameTest.getAmountOfPins());
+        Assert.assertEquals("amount of players should be 0", 0, gameTest.getAmountOfPlayers());
+        Assert.assertEquals("players list should be empty", new ArrayList<>(), gameTest.getListPlayers());
+        Assert.assertEquals("scores map should be empty and of type <Player, Score>", new HashMap<Player, Score>(), gameTest.getScores());
+        Assert.assertEquals("scores map should be empty and of type <Player, int[][]>", new HashMap<Player, int[][]>(), gameTest.getPinsMap());
     }
 
     @Test
-    public void testNoPlayersAtStartOfGame() {
+    public void testSetters() {
         Game gameTest = new Game();
-        assertEquals(gameTest.getAmountOfPlayers(), 0);
-        assertEquals(gameTest.getListNames().size(), 0);
-        assertEquals(gameTest.getPlayersScores(), 0);
+        gameTest.setStarted(true);
+        assertTrue("started boolean should be true", gameTest.getStarted());
+        gameTest.setAmountOfTurns(3);
+        Assert.assertEquals("amount of turns should be 3", 3, gameTest.getAmountOfTurns());
+        gameTest.setAmountOfPins(4);
+        Assert.assertEquals("amount of pins should be 4", 4, gameTest.getAmountOfPins());
+        gameTest.setAmountOfPlayers(5);
+        Assert.assertEquals("amount of players should be 5", 5, gameTest.getAmountOfPlayers());
     }
 
     @Test
     public void testAddPlayer() {
         Game gameTest = new Game();
-        gameTest.setStarted(true);
-        gameTest.addPlayer("Olivier");
-        ArrayList<String> emptyList = new ArrayList<>();
-
-        assertEquals("Only Olivier in listNames", emptyList, gameTest.getListNames());
-        emptyList.add("Olivier");
-        assertEquals("Only Olivier in listNames", gameTest.getListNames().size(), 1);
-        assertEquals("initial Olivier score should be 0", gameTest.getPlayerScore("Olivier"), 0);
-        gameTest.addScore("Olivier", 10);
-        assertEquals("Olivier's score should be 10", gameTest.getPlayerScore("Olivier"), 10);
-        System.out.println(gameTest.getPlayerScore("Olivier"));
+        gameTest.setAmountOfTurns(10);
+        gameTest.addPlayer("testPlayer");
+        Assert.assertEquals("list size should not be 0", 1, gameTest.getListPlayers().size());
+        Player testPlayer = gameTest.getListPlayers().get(0);
+        Assert.assertEquals("name should be testPlayer", "testPlayer", testPlayer.getName());
+        Assert.assertEquals("amount of turns should be 10", 10, testPlayer.getTurns());
     }
 
     @Test
-    public void xxxtestAddPlayer() {
-        Game gameTest = new Game();
-        //assertEquals();
+    public void testisInt() {
+        assertTrue("'1' should be considered as an int", isInt("1"));
+        assertFalse("'1a' should not be considered as an int", isInt("1a"));
+    }
+
+    @Test
+    public void testGetNewArray() {
+        int[][] compareArray = new int[2][2];
+        compareArray[0][0] = 0;
+        compareArray[0][1] = 0;
+        compareArray[1][0] = 0;
+        compareArray[1][1] = 0;
+        Assert.assertEquals("getNewArray should return an empty array", compareArray, getNewArray(2));
+    }
+
+    @Test
+    public void testRetrieveInt() { //TODO find a way to send text through System.in --> ask Charly Schmidt at next standup (19/01/2024)
+    }
+
+    @Test
+    public void testIntArrayToIntegerArray() {
+        int[][] compareIntArray = new int[2][2];
+        Integer[][] compareIntegerArray = new Integer[2][2];
+        for(int x = 0; x < 2; x++){
+            for(int y = 0; y < 2; y++) {
+                compareIntArray[x][y] = 0;
+                compareIntegerArray[x][y] = 0;
+            }
+        }
+        Assert.assertEquals("compareIntArray should become an integerArray", compareIntegerArray, intArrayToIntegerArray(compareIntArray));
+    }
+
+    @Test
+    public void testPlay() { //TODO find a way to send text through System.in --> ask Charly Schmidt at next standup (19/01/2024)
     }
 
 }
